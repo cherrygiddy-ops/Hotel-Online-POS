7d0fa3582ce12196384715a1394c4837fc559059
Morris_Cherry <morrischerryg@gmail.com>
2026-05-01 18:30:51 +0300

    updated monthly api service

--- src/services/RevenueService.ts
+++ src/services/RevenueService.ts
@@ -1,4 +1,4 @@
 import { MonthlyRevenue } from "@/entities/MonthlyRevenue";
 import APICLIENT from "./ApiClient";
 
-export default new APICLIENT<unknown, MonthlyRevenue>("api/revenue/monthly");
+export default new APICLIENT<unknown, MonthlyRevenue>("/revenue/monthly");
