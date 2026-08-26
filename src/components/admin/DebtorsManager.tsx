34c3242999d1ae4c283e8f9e2a5ef99b41e09fa2
cherrygiddy-ops <morrischerryg@gmail.com>
2026-08-26 18:51:00 +0300

    ..............

--- src/components/admin/DebtorsManager.tsx
+++ src/components/admin/DebtorsManager.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
+
 interface Receipt {
   receiptNo: number;
   items: string[];
