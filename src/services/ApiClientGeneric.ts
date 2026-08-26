2475da10411999f53f47e96c97e5731522986bd8
cherrygiddy-ops <morrischerryg@gmail.com>
2026-08-26 12:23:10 +0300

    remove generics

--- src/services/ApiClientGeneric.ts
+++ src/services/ApiClientGeneric.ts
@@ -1,33 +1,33 @@
-import axios, { type AxiosRequestConfig } from "axios";
-import { OrderSummaryDto } from "@/entities/OrderSummaryDto";
-import { useAuthStore } from "@/Store/AuthStore";
-import { CheckoutRequestDto } from "@/entities/CheckoutRequestDto";
-import { CheckoutResponseDto } from "@/entities/CheckoutResponseDto";
-import { axiosInstance } from "./ApiClient";
-class APICLIENT<TRequest, TResponse> {
-  endpoint: string;
+// import axios, { type AxiosRequestConfig } from "axios";
+// import { OrderSummaryDto } from "@/entities/OrderSummaryDto";
+// import { useAuthStore } from "@/Store/AuthStore";
+// import { CheckoutRequestDto } from "@/entities/CheckoutRequestDto";
+// import { CheckoutResponseDto } from "@/entities/CheckoutResponseDto";
+// import { axiosInstance } from "./ApiClient";
+// class APICLIENT<TRequest, TResponse> {
+//   endpoint: string;
 
-  constructor(endpoint: string) {
-    this.endpoint = endpoint;
-  }
+//   constructor(endpoint: string) {
+//     this.endpoint = endpoint;
+//   }
 
-  // Generic GET all
-  getAll = (config?: AxiosRequestConfig) =>
-    axiosInstance.get<TResponse[]>(this.endpoint, config).then(res => res.data);
+//   // Generic GET all
+//   getAll = (config?: AxiosRequestConfig) =>
+//     axiosInstance.get<TResponse[]>(this.endpoint, config).then(res => res.data);
 
-  // Generic GET by id
-  get = (id: string | number, config?: AxiosRequestConfig) =>
-    axiosInstance.get<TResponse>(`${this.endpoint}/${id}`, config).then(res => res.data);
+//   // Generic GET by id
+//   get = (id: string | number, config?: AxiosRequestConfig) =>
+//     axiosInstance.get<TResponse>(`${this.endpoint}/${id}`, config).then(res => res.data);
 
-  // Generic POST
-  post = (path: string = "", body?: TRequest, config?: AxiosRequestConfig) =>
-    axiosInstance.post<TResponse>(`${this.endpoint}${path}`, body, config).then(res => res.data);
+//   // Generic POST
+//   post = (path: string = "", body?: TRequest, config?: AxiosRequestConfig) =>
+//     axiosInstance.post<TResponse>(`${this.endpoint}${path}`, body, config).then(res => res.data);
 
-  // Generic PUT
-  put = (path: string, body?: Partial<TRequest>, config?: AxiosRequestConfig) =>
-    axiosInstance.put<TResponse>(`${this.endpoint}${path}`, body, config).then(res => res.data);
+//   // Generic PUT
+//   put = (path: string, body?: Partial<TRequest>, config?: AxiosRequestConfig) =>
+//     axiosInstance.put<TResponse>(`${this.endpoint}${path}`, body, config).then(res => res.data);
 
-  // Generic DELETE
-  delete = (path: string, config?: AxiosRequestConfig) =>
-    axiosInstance.delete<TResponse>(`${this.endpoint}${path}`, config).then(res => res.data);
-}
+//   // Generic DELETE
+//   delete = (path: string, config?: AxiosRequestConfig) =>
+//     axiosInstance.delete<TResponse>(`${this.endpoint}${path}`, config).then(res => res.data);
+// }
